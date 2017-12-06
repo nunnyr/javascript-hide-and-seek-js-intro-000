e27e83b8d5758105e04ae00dca66bf11a898d78b
Samantha R <nunnyreyes@gmail.com>
2017-12-06 01:02:08 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,37 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
+
+function nestedTarget() {
+  return document.querySelector('#nested .target')
+}
+
+function increaseRankBy(n) {
+  const rankedLists = document.querySelectorAll('.ranked-list')
+
+  for (let i = 0, l = rankedLists.length; i < l; i++) {
+    let children = rankedLists[i].children
+
+    for (let j = 0, k = children.length; j < k; j++) {
+      children[j].innerHTML = parseInt(children[j].innerHTML) + n
+    }
+  }
+}
+//
+// function deepestChild() {
+//   let node = document.getElementById('grand-node')
+//   let nextNode = node.children[0]
+//
+//   while (nextNode) {
+//     node = nextNode
+//     nextNode = node.children[0]
+//   }
+// }
+function deepestChild() {
+  var lis=document.getElementById("grand-node").querySelectorAll("div");
+  var test;
+  for(let i=0;i<lis.length-1;i++) {
+    test=lis[i].querySelector("div");
+  }
+return test;
+}
