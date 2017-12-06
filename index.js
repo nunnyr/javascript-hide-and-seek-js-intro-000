7f210a51ef80492bd0d6892cc76b476816f64dcb
Samantha R <nunnyreyes@gmail.com>
2017-12-06 01:13:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
+//Define a function getFirstSelector(selector), which accepts a selector 
+//and returns the first element that matches.
+
 function getFirstSelector(selector) {
   return document.querySelector(selector);
 }
 
+//Define a function nestedTarget() that pulls a .target out of #nested (# is used for IDs in selectors — but you knew that because you read the docs, right? :) ). (Note that in index.html #nested 
+//and .target just happen to be divs. 
+//This method should work with arbitrary elements.)
 function nestedTarget() {
   return document.querySelector('#nested .target')
 }
 
+
+//Define a function increaseRankBy(n) that increases the ranks in all 
+//of the .ranked-lists by n. (You might need to make use of parseInt()
 function increaseRankBy(n) {
   const rankedLists = document.querySelectorAll('.ranked-list')
 
@@ -17,16 +26,11 @@ function increaseRankBy(n) {
     }
   }
 }
-//
-// function deepestChild() {
-//   let node = document.getElementById('grand-node')
-//   let nextNode = node.children[0]
-//
-//   while (nextNode) {
-//     node = nextNode
-//     nextNode = node.children[0]
-//   }
-// }
+//Define a function deepestChild() that pulls out the most deeply nested child 
+//from div#grand-node. (Remember, you can iterate over elements and call 
+//querySelector() and querySelectorAll() on them. This is challenging to 
+//implement correctly, but not beyond your ability!)
+
 function deepestChild() {
   var lis=document.getElementById("grand-node").querySelectorAll("div");
   var test;
